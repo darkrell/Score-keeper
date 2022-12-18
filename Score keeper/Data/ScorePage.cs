edfcc092b5b1913324549054cbb4396f5983e92f
darkrell <rc-alh@outlook.com>
2022-12-18 17:59:17 +0300

    Completed logic

--- Score keeper/Data/ScorePage.cs
+++ Score keeper/Data/ScorePage.cs
@@ -1,6 +1,11 @@
 namespace Score_keeper.Data;
 
-public class ScorePage
+public class ScorePageModel
 {
+    public string Name { get; set; } = DateTime.Now.ToString("g");
+    public int PlayersCount { get; set; } = 2;
 
+    public int ScoreHightChangeValue { get; set; } = 10;
+    public int ScoreChangeValue { get; set; } = 5;
+    public int[] Scores { get; set; } = new int[20];
 }
